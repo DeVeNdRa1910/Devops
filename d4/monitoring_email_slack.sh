0d07d43ac8adea014724abbe9664178641cf8f45
Devendra <jaybantv@gmail.com>
2025-03-13 20:26:01 +0530

    just learn basic things of shell script and create server monitoring and sending alert on email and slack

--- d4/monitoring_email_slack.sh
+++ d4/monitoring_email_slack.sh
@@ -2,25 +2,30 @@
 
 host="localhost"
 port=5000
-check_url="http://localhost:5000/api/health"
+slack_webhook_url="https://hooks.slack.com/services/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"
+check_url="http://localhost:5000/"
 log_file="/var/log/automate_server.log"
 alert_email="jaybantv@gmail.com"
-threshold=3
 retry_interval=5
 failure_count=0
 timestamp=$(date)
 failure_count_file="/tmp/server_failure_count"
-
-
+threshold=3
 if [ -f "$failure_count_file" ]; then
     failure_count=$(cat "$failure_count_file")
 else
     failure_count=0
 fi
 
-send_alert() {
+send_slack_alert(){
+        local message="Warning: $check_url is DOWN!"
+        local payload="{\"text\":\"${message}\"}"
+        curl -X POST -H 'Content-type: application/json' --data "$payload" "$slack_webhook_url"> /dev/null 2>&1
+}
+
+send_email_alert() {
     timestamp=$(date)
-    echo -e "Subject: Server Down Alert\n\nHello, $host:$port is down after $threshold failures." | msmtp -a mail "$alert_email" 2>&1 | tee -a "$log_file"
+    echo -e "Subject: Server Down Alert\n\nHello, $host:$port is down after 3 failures." | msmtp -a mail "$alert_email" 2>&1 | tee -a "$log_file"
 
     if [ $? -eq 0 ]; then
         echo "$timestamp: Email sent successfully" >> "$log_file"
@@ -31,13 +36,14 @@ send_alert() {
 
 perform_check(){
     timeout 5 bash -c "echo > /dev/tcp/$host/$port" > /dev/null 2>&1
-    
+
     if [ $? -ne 0 ] ; then
                 failure_count=$((failure_count+1))
                 echo "$failure_count" > "$failure_count_file"
                 echo "$timestamp: $host:$port check failed ($failure_count failures)." >> "$log_file"
                 if [ "$failure_count" -ge "$threshold" ] ; then
-                        send_alert
+                        send_email_alert
+			send_slack_alert 
                         failure_count=0
                 fi
     else
--- d4/server_monitoring_slack.sh
+++ d4/server_monitoring_slack.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-slack_webhook_url="https://hooks.slack.com/services/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"
+slack_webhook_url="https://hooks.slack.com/services/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"
 host_to_monitor="http://localhost:5050/api/books/get-all-books"
-check_interval=10
+check_interval=1
 log_file="/var/log/server_monitoring.log"
 
 send_slack_notification(){
 	local message="$1"
-	curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$slack_webhook_url" > /dev/null 2>&1
+	local payload="{\"text\":\"${message}\"}"
+	local response
+
+	response=$(curl -X POST -H 'Content-type: application/json' --data "$payload" "$slack_webhook_url" -w %{http_code}> /dev/null 2>&1)
+
+	local status_code=${response: -3}
+
+	if [[ $status_code -eq 200 ]]; then
+		echo "$(date) - Alert notification delivered successfully with status_code $status_code" >> "$log_file"
+	else
+		echo "$(date) - Alert notification is not delivered with status_code $status_code" >> "$log_file"
+	fi
 }
 
 #function for cehcking, host is up or down
@@ -22,7 +33,7 @@ check_uptime(){
 		echo "$(date) - $host_to_monitor is UP, with status code $status_code" >> "$log_file"
 	else
 		echo "$(date) - $host_to_monitor is DOWN, with status code $status_code" >> "$log_file"
-		send_slack_notification "*Warning:* $host_to_monitor is down!, with status code $status_code "
+		send_slack_notification "*Warning:* $host_to_monitor is down!, with status code $status_code"
 	fi
 }
 
